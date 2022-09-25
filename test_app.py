bba79adb461f0f17861ed3c32a12d9a5533c571e
R. Da Costa <107809125+dacostaration@users.noreply.github.com>
2022-09-25 16:00:13 -0400

    Added 2nd test

--- test_app.py
+++ test_app.py
@@ -8,3 +8,11 @@ from application import app
 def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
+    
+def test_your_url():
+    res = "http://deployment02-main-dev.us-east-2.elasticbeanstalk.com/gg"   # if we wanted to compare the result of the "shortened" url, this is what is should be
+    response = app.test_client().post("/your-url", data={
+        "code": "gg",
+        "url": "http://wwww.google.com",
+    })
+    assert response.status_code == 200    
